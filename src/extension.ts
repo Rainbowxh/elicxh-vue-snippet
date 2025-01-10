1163eceb7b87653f15c5a5b8d92e9658c53bad44
Rainbowxh <elicxh@gmail.com>
2025-01-10 10:24:00 +0800

    refactor: refactor code;

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,8 @@ import { initImport, initProvider } from './provider';
 
 export function activate(context: vscode.ExtensionContext) {
   // 注册一个提供补全项的命令
-  const provider = initProvider();
-  // 注册一个命令来添加 import 语句
-  const addImportCommand = initImport();
-  context.subscriptions.push(provider, addImportCommand);
+  context.subscriptions.push(initProvider());
+  context.subscriptions.push(initImport());
 }
 
 export function deactivate() { }
